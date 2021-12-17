8b73896c52589b71489969aa08d04338d4f7f642
levvint <84582879+levvint@users.noreply.github.com>
2021-12-17 22:55:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 #!/bin/env python3 
+## groupe : Levent Vincent , Teruel Benjamin
+
 from Prisme import *
 from config import *
 
 
-'''
-un_prisme = Prisme('Nuage')
 
+
+un_prisme = Prisme('Nuage')
 
 '''
 
 un_prisme = Prisme('Image')
 
+'''
 
 un_prisme.run(urls)
+
 un_prisme.show()
